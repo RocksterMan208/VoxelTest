df881a5f694bce7714bff127f70d307b6ed99ce1
RocksterMan208 <troniclife12@gmail.com>
2026-01-22 12:23:25 +0000

    Added in LearnOpenGL's shader class to make my own life easier and added the coloured triangle. TRIANGLE TRIANGLE!

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,6 @@
 #include "settings.hpp"
 #include "inputs.hpp"
-
-const char *vertexShaderSource = "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "void main()\n"
-    "{\n"
-    " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "}\0";
-
-
-const char *fragmentShaderSource = "#version 330 core\n"
-    "out vec4 FragColor;\n"
-    "void main()\n"
-    "{\n"
-    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    "}\n\0";
-
+#include "shader.hpp"
 
 int main()
 {
@@ -31,10 +16,12 @@ int main()
 
     GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Voxel", NULL, NULL);
 
-    float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, 0.5f, 0.0f,
-        0.0f, 0.5f, 0.0f
+    float vertices[] = 
+    {
+        // positions         // colors
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
     };
 
     glfwMakeContextCurrent(window);
@@ -46,76 +33,36 @@ int main()
     }
 
 
-// Shader stuff -------------------- (please move to a separate header file at some point and make the shaders actually reference a file instead of just having ts as a variable.)
-    // Vertex shader:
-
-    unsigned int vertexShader;
-    vertexShader = glCreateShader(GL_VERTEX_SHADER);
-
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "Vertex shader failed to compile!!" << infoLog << std::endl;
-    }
-
-
-    // Fragment shader
-    
-
-    
-    unsigned int fragmentShader;
-    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "Fragment shader failed to compile!!" << infoLog << std::endl;
-    }
-
-    // Creating program
-    unsigned int shaderProgram;
-    shaderProgram = glCreateProgram();
-
-    // Linking shader attributes.
-
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    
-    if (!success) 
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "Shader Linking failed.\n" << infoLog << std::endl;
-    }
-
-    // Using the program then deleting unnecessary shaders to avoid overuse of memory
-
-    glUseProgram(shaderProgram);
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    Shader shaders
+    (
+        "resources/shaders/vertex_shader.glsl",
+        "resources/shaders/fragment_shader.glsl"
+    );
 
     // Doing viewports
 
     glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
     unsigned int VAO, VBO;
+
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    
 
     // Frame loop
 
@@ -123,11 +70,16 @@ int main()
     {
         // Logic
         getInputs(window);
+        
 
         // Rendering
 
         glClearColor(0.0f, 0.33f, 0.33f, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        shaders.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // buffer swapping and shi
         glfwSwapBuffers(window);
